2d14449b3aacf6c857b74974ada4c2488d439e4f
Dmytro Andoniev <dmytro.andoniev@rztech.us>
2023-06-03 12:46:57 +0300

    Added aliases maintainance

---
+++ tests/lib/rules/fsd-path-checker.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview validation of imports for fsd methodology
+ * @author Dima
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/fsd-path-checker"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: "module" },
+});
+
+ruleTester.run("fsd-path-checker", rule, {
+  valid: [
+    {
+      filename: "C:\\Users\\dando\\Desktop\\Pet_Project\\src\\entities\\Article\\ui\\Article.tsx",
+      code: "import { addCommentFormActions } from \"../../model/slices/addCommentForm\";",
+      errors: [{ message: "Imports should be relative in scope of one slice"}],
+    },
+  ],
+
+  invalid: [
+    {
+      filename: "C:\\Users\\dando\\Desktop\\Pet_Project\\src\\entities\\Article\\ui\\Article.tsx",
+      code: "import { addCommentFormActions } from \"@/entities/Article/model/slices/addCommentForm\";",
+      errors: [{ message: "Imports should be relative in scope of one slice" }],
+      options: [{ alias: "@"}]
+    },
+  ],
+});
